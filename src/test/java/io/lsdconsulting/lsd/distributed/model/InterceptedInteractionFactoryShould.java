46d050dfdf5bf648fb5e8f05f6e859db687434f7
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2021-10-12 17:45:44 +0100

    Unit tests added for the factory

--- src/test/java/io/lsdconsulting/lsd/distributed/model/InterceptedInteractionFactoryShould.java
+++ src/test/java/io/lsdconsulting/lsd/distributed/model/InterceptedInteractionFactoryShould.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static org.apache.commons.lang3.RandomUtils.nextLong;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -16,21 +17,25 @@ class InterceptedInteractionFactoryShould {
     private final String profile = randomAlphanumeric(10);
     private final String body = randomAlphanumeric(30);
     private final String traceId = randomAlphanumeric(30);
-    private final Map<String, Collection<String>> headers = Map.of(randomAlphanumeric(10), List.of(randomAlphanumeric(10)));
+    private final Map<String, Collection<String>> requestHeaders = Map.of(randomAlphanumeric(10), List.of(randomAlphanumeric(10)));
+    private final Map<String, Collection<String>> responseHeaders = Map.of(randomAlphanumeric(10), List.of(randomAlphanumeric(10)));
     private final String serviceName = randomAlphanumeric(30);
     private final String target = randomAlphanumeric(30);
     private final String path = randomAlphanumeric(30);
+    private final String httpStatus = randomAlphanumeric(30);
+    private final String httpMethod = randomAlphanumeric(30);
+    private final Long elapsedTime = nextLong();
     private final Type type = Type.values()[RandomUtils.nextInt(0,Type.values().length)];
 
     private final InterceptedInteractionFactory underTest = new InterceptedInteractionFactory(profile);
 
     @Test
-    void test() {
-        InterceptedInteraction result = underTest.buildFrom(body, traceId, headers, serviceName, target, path, type);
+    void buildWithNoElapsedTimeRequestAndResponseHeadersHttpStatusAndHttpMethod() {
+        InterceptedInteraction result = underTest.buildFrom(body, traceId, requestHeaders, serviceName, target, path, type);
 
         assertThat(result.getBody(), is(body));
         assertThat(result.getTraceId(), is(traceId));
-        assertThat(result.getRequestHeaders(), is(headers));
+        assertThat(result.getRequestHeaders(), is(requestHeaders));
         assertThat(result.getServiceName(), is(serviceName));
         assertThat(result.getTarget(), is(target));
         assertThat(result.getPath(), is(path));
@@ -42,5 +47,45 @@ class InterceptedInteractionFactoryShould {
         assertThat(result.getHttpMethod(), is(nullValue()));
         assertThat(result.getHttpStatus(), is(nullValue()));
         assertThat(result.getResponseHeaders(), is(anEmptyMap()));
+    }
+
+    @Test
+    void buildWithNoElapsedTimeAndResponseHeaders() {
+        InterceptedInteraction result = underTest.buildFrom(body, requestHeaders, traceId, serviceName, target, path, httpStatus, httpMethod, type);
+
+        assertThat(result.getBody(), is(body));
+        assertThat(result.getTraceId(), is(traceId));
+        assertThat(result.getRequestHeaders(), is(requestHeaders));
+        assertThat(result.getServiceName(), is(serviceName));
+        assertThat(result.getTarget(), is(target));
+        assertThat(result.getPath(), is(path));
+        assertThat(result.getProfile(), is(profile));
+        assertThat(result.getType(), is(type));
+        assertThat(result.getHttpMethod(), is(httpMethod));
+        assertThat(result.getHttpStatus(), is(httpStatus));
+
+        assertThat(result.getCreatedAt(), is(not(nullValue())));
+        assertThat(result.getElapsedTime(), is(nullValue()));
+        assertThat(result.getResponseHeaders(), is(anEmptyMap()));
+    }
+
+    @Test
+    void buildInterceptedInteractionWithAllProperties() {
+        InterceptedInteraction result = underTest.buildFrom(body, requestHeaders, responseHeaders, traceId, serviceName, target, path, httpStatus, elapsedTime, httpMethod, type);
+
+        assertThat(result.getBody(), is(body));
+        assertThat(result.getTraceId(), is(traceId));
+        assertThat(result.getRequestHeaders(), is(requestHeaders));
+        assertThat(result.getResponseHeaders(), is(responseHeaders));
+        assertThat(result.getServiceName(), is(serviceName));
+        assertThat(result.getTarget(), is(target));
+        assertThat(result.getPath(), is(path));
+        assertThat(result.getProfile(), is(profile));
+        assertThat(result.getType(), is(type));
+        assertThat(result.getHttpMethod(), is(httpMethod));
+        assertThat(result.getHttpStatus(), is(httpStatus));
+        assertThat(result.getElapsedTime(), is(elapsedTime));
+
+        assertThat(result.getCreatedAt(), is(not(nullValue())));
     }
 }
