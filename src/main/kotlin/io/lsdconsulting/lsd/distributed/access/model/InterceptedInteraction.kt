b30610f91c97f15e3609b3218c2bb628175736f9
Lukasz Gryzbon <lukasz.gryzbon@gmail.com>
2023-05-04 15:07:27 +0100

    fix: Make header fields consistent

--- src/main/kotlin/io/lsdconsulting/lsd/distributed/access/model/InterceptedInteraction.kt
+++ src/main/kotlin/io/lsdconsulting/lsd/distributed/access/model/InterceptedInteraction.kt
@@ -5,8 +5,8 @@ import java.time.ZonedDateTime
 data class InterceptedInteraction(
     val traceId: String,
     val body: String? = null,
-    val requestHeaders: MutableMap<String, Collection<String>> = mutableMapOf(),
-    val responseHeaders: Map<String, Collection<String>> = mutableMapOf(),
+    val requestHeaders: Map<String, Collection<String>> = mapOf(),
+    val responseHeaders: Map<String, Collection<String>> = mapOf(),
     val serviceName: String, // the calling service or the publisher or consumer
     val target: String, // the called service or the exchange/topic name
     val path: String, // the called URL or the exchange/topic name
